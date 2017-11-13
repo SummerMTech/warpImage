e5778d212ce2b239493015a6cab31f024c258580
Adasnaa <dan.rcr01@gmail.com>
2017-11-13 00:03:38 -0300

    need files  to prove the image warp python script

---
+++ backProve.py
@@ -0,0 +1,40 @@
+import numpy as np
+import cv2
+
+class Detector():
+    """this class need begin the user with the polygon data, and have two methods, both methods need the frame 
+       and are called "cutRegion" and "findCar", the first return a image cut in basis to polygon and the second
+       return the coordinates of the box from the car detected"""
+
+    def __init__(self):
+        self.kernel = cv2.getStructuringElement(cv2.MORPH_RECT,(5,5))
+        self.fgbg = cv2.createBackgroundSubtractorMOG2()
+    
+    def findCar(self,frame):
+        #Cortado = Perspective(np.array(lista))
+        #cortar = Cortado._warpOp(frame)
+        gris=cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        detect=self.car.detectMultiScale(gris, 1.1, 2)
+        #rectangle.append((detect))
+
+        return detect
+    def get_centroid(x, y, w, h):
+        x1 = int(w / 2)
+        y1 = int(h / 2)
+        cx = x + x1
+        cy = y + y1
+        return (cx, cy)
+    def findBackSubs(self,frame):
+        matches=[]
+        fgmask = self.fgbg.apply(frame)
+        fgmask = cv2.morphologyEx(fgmask, cv2.MORPH_OPEN, self.kernel)
+        #cv2.imshow('frame',fgmask)
+        im2, contours, hierarchy = cv2.findContours(fgmask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_TC89_L1)
+        for (i, contour) in enumerate(contours):
+            (x, y, w, h) = cv2.boundingRect(contour)
+            centroid = Detector.get_centroid(x, y, w, h)####looking for the best way
+            matches.append(((x, y, w, h), centroid))
+        #print(len(matches))    
+        return matches
+
+
--- pruebaToCut.py
+++ pruebaToCut.py
@@ -2,18 +2,31 @@ import numpy as np
 import cv2
 from cutImage import Transform
 import timeit
+from backProve import Detector
 lista=[]
-
+boxes=[]
+datos=np.load('D:/mine/raspberry/python/officialTrialVideos/sar.npy')
+listaFile=datos[1]
+print(datos)
 def get_BigRectangle(event,x,y,flags,param):
     if event == cv2.EVENT_LBUTTONDOWN:
         lista.append((x,y))
         if len(lista)!=0:
             cv2.circle(frame, (x,y),3,(0,255,0),-1)
             cv2.imshow('First_Frame',frame)
+
+def draw(matches,frame_resized):
+		for (i, match) in enumerate(matches):
+			contour, centroid = match[0], match[1]
+			x,y,w,h = contour
+			cv2.rectangle(frame_resized, (x,y),(x+w-1, y+h-1),(0,0,255),1)
+			cv2.circle(frame_resized, centroid,2,(0,255,0),-1)
+			
+		
     
 try:
 		#cap=cv2.VideoCapture(directorioDeVideos+nameSourceVideo)
-		cap=cv2.VideoCapture('officialTrialVideos/sar.mp4')
+		cap=cv2.VideoCapture('D:/mine/raspberry/python/officialTrialVideos/sar.mp4')
 		for i in range (50):
 			ret, frame=cap.read()
 		frame=cv2.resize(frame,(320,240))
@@ -37,8 +50,8 @@ while True:
 		#print('ListaAux Removed...'+ str(listaAux))
 		overlay=frame.copy()
 	if keyPress == 27:
-		print('_____Data not accept..')
-		print('*Select two points next press -a- to obtain Angle')
+		#print('_____Data not accept..')
+		#print('*Select two points next press -a- to obtain Angle')
 		#print('lista no append: '+str(lista))
 		lista=[]
 		#print('ListaAux Removed...'+ str(listaAux))
@@ -50,19 +63,29 @@ while True:
 		print('saved!!')
 		break
 cv2.destroyAllWindows()
-
-cutting=Transform(lista)
-cap=cv2.VideoCapture('officialTrialVideos/sar.mp4')
+backSub=Detector()
+#cutting=Transform(lista)
+cutting=Transform(listaFile)
+cap=cv2.VideoCapture('D:/mine/raspberry/python/officialTrialVideos/sar.mp4')
 while(1):
 	ret, frame=cap.read()
 	frame=cv2.resize(frame,(320,240))
 	start=timeit.default_timer()	
 	cutIma=cutting.cutRegion(frame)
+	Boxes=backSub.findBackSubs(cutIma)
+	Real_Boxes=cutting.real_Points(Boxes)
+	draw(Boxes,cutIma)
+	draw(Real_Boxes,frame)
+	#print('puntos')
+	#print(Boxes)
+	#print('puntosReales')
+	#print(Real_Boxes)
 	lapso=timeit.default_timer()
 	tim=lapso-start
-	print(tim)
+	#print(tim)
 	cv2.imshow('original',frame)
 	cv2.imshow('cutting',cutIma)
+	#print('data:...'+str(boxes))
 	if cv2.waitKey(1)&0xFF==ord('q'):
 		# file2.write("\n".listFinal)         
 		print('saved!!')
